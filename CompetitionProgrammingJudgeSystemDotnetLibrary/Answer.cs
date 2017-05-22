b45a20df36179071c3f4586d7a2529062cf9d7f7
Maisu <mystasly48@gmail.com>
2017-05-22 21:26:19 +0900

    Added documentation

--- CompetitionProgrammingJudgeSystemDotnetLibrary/Answer.cs
+++ CompetitionProgrammingJudgeSystemDotnetLibrary/Answer.cs
@@ -1,13 +1,25 @@
 ﻿using Newtonsoft.Json;
 
 namespace CompetitionProgrammingJudgeSystemDotnetLibrary {
+  /// <summary>
+  /// Answer Json Payload
+  /// </summary>
   public class Answer {
+    /// <summary>
+    /// Question ID
+    /// </summary>
     [JsonProperty("id")]
     public int Id { get; set; }
 
+    /// <summary>
+    /// Answer Code
+    /// </summary>
     [JsonProperty("code")]
     public string Code { get; set; }
 
+    /// <summary>
+    /// Answer Language
+    /// </summary>
     [JsonProperty("lang")]
     public Language Language { get; set; }
   }
--- CompetitionProgrammingJudgeSystemDotnetLibrary/Language.cs
+++ CompetitionProgrammingJudgeSystemDotnetLibrary/Language.cs
@@ -1,9 +1,18 @@
 ﻿using System.Runtime.Serialization;
 
 namespace CompetitionProgrammingJudgeSystemDotnetLibrary {
+  /// <summary>
+  /// Programming Language Enums
+  /// </summary>
   public enum Language {
+    /// <summary>
+    /// Programming Language of Ruby
+    /// </summary>
     [EnumMember(Value = "rb")]
     Ruby,
+    /// <summary>
+    /// Programming Language of Python 2
+    /// </summary>
     [EnumMember(Value = "py")]
     Python
   }
